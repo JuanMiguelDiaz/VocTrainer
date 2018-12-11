b889d01f28e990b028ea37b5ad77bde4ccfc66b8
Jean Michel Diaz <je.mic.diaz@gmail.com>
2018-12-11 18:36:26 +0100

    Taking a quiz in your Terminal now works :PARTY:

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 from datetime import date
 from datetime import time
 from datetime import datetime
+from datetime import timedelta
 
 #----------------------Setting variables-------------------------
 
@@ -46,29 +47,32 @@ def setUp():
 	for key, value in GlobalDict.items():
 		if (value['DueDate'] == ''):
 			pass
-		elif (today >= datetime.strptime((value["DueDate"]+' 00:00:00'),"%d.%m.%y %H:%M:%S")):
+		elif (today >= datetime.strptime((value["DueDate"]),"%d.%m.%y")):
 			if value["Subject"] in ListofSubjects.keys() :
 				ListofSubjects[value["Subject"]] += 1
 			else: 
 				ListofSubjects[value["Subject"]] = 1
 
-	# Get user input
-	print("Subject overview:")
-	for key, value in ListofSubjects.items() :
-		print(key, value)
-	
-	chosenSubject = raw_input('Which subject do you want to learn: ') #Wie kann man Auswahlmöglichkeiten geben?
-	message = "Alright, let's start with {}!".format(chosenSubject)
-	print (message)
+	# Check if any items are due
+	if len(ListofSubjects.keys()) == 0:
+		print('You are all good - no due items today!')
+	else:
+		print("Subject overview:")
+		for key, value in ListofSubjects.items() :
+			print(key, value)
+		
+		chosenSubject = raw_input('Which subject do you want to learn: ') #Wie kann man Auswahlmöglichkeiten geben?
+		message = "Alright, let's start with {}!".format(chosenSubject)
+		print (message)
 
-	# Generate list of openQuestions
-	for key, value in GlobalDict.items():
-		if (value['DueDate'] == ''):
-			pass
-		elif (value['Subject'] == chosenSubject) and (today >= datetime.strptime((value["DueDate"]+' 00:00:00'),"%d.%m.%y %H:%M:%S")):
-			openQuestions.append(key)
-	message = "Quiz with {} items is generated.".format(len(openQuestions))
-	print(message)
+		# Generate list of openQuestions
+		for key, value in GlobalDict.items():
+			if (value['DueDate'] == ''):
+				pass
+			elif (value['Subject'] == chosenSubject) and (today >= datetime.strptime((value["DueDate"]+' 00:00:00'),"%d.%m.%y %H:%M:%S")):
+				openQuestions.append(key)
+		message = "Quiz with {} items is generated.".format(len(openQuestions))
+		print(message)
 
 
 def askRandomQuestion():
@@ -76,20 +80,46 @@ def askRandomQuestion():
 	global GlobalDict
 	global QuizOn
 
+	def updateDueDate(currentPhase):
+		if currentPhase == 1:
+			return datetime.now() + timedelta(days=3)
+		elif currentPhase == 2:
+			return datetime.now() + timedelta(days=10)
+		elif currentPhase == 3:
+			return datetime.now() + timedelta(days=30)
+		elif currentPhase == 4:
+			return datetime.now() + timedelta(days=30)
+		elif currentPhase == 5:
+			return datetime.now() + timedelta(days=90)
+		elif currentPhase == 6:
+			return ''
+
 	currentQuestion = openQuestions[random.randint(0,len(openQuestions)-1)]
 	print("---------------------------------------------------------------------")
-	print GlobalDict[currentQuestion]["Frage"]
-	raw_input('Your answer: ')
-	print 'Correct answer: ' + GlobalDict[currentQuestion]["Antwort"]
+	if GlobalDict[currentQuestion]["Tausch"] == "" :
+		print GlobalDict[currentQuestion]["Frage"]
+		raw_input('Your answer: ')
+		print 'Correct answer: ' + GlobalDict[currentQuestion]["Antwort"]
+	else:
+		print GlobalDict[currentQuestion]["Antwort"]
+		raw_input('Your answer: ')
+		print 'Correct answer: ' + GlobalDict[currentQuestion]["Frage"]
+	
 	Check = raw_input('Was your answer correct? Type "y" for Yes, "e" for exit: ')
 	
 	if Check == "y" :
+		GlobalDict[currentQuestion].update({'DueDate': updateDueDate(int(GlobalDict[currentQuestion]["Phase"])).strftime("%d.%m.%y")}) #["Phase"]).strptime("%d.%m.%y")
+		newPhase = int(GlobalDict[currentQuestion]['Phase'])+1
+		GlobalDict[currentQuestion].update({'Phase': newPhase})
 		openQuestions.remove(currentQuestion)
+		message = 'Nice, new due date is {}'.format(GlobalDict[currentQuestion]["DueDate"])
+		print(message)
 		# Needs to delete right items from openQuestions and update the GlobalDict
 	elif Check == "e":
 		QuizOn = 0
 	else:
 		GlobalDict[currentQuestion]['Phase'] = 1
+		print 'Oops, item returned to phase 1.'
 
 
 def saveNewCSV ():
